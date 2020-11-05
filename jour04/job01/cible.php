9b2427e7e7fd4a395a2f971de8a921bf9f6085ff
emmanuel cabassot <emmanuel.cabassot@laplateforme.io>
2020-11-05 15:43:22 +0100

    ..

--- jour04/job01/cible.php
+++ jour04/job01/cible.php
@@ -1,10 +1,11 @@
 <?php
-
+//Entrée les valeurs de GET dans un tableau
 foreach ($_GET as $key => $value) {
     $table[] = $key;
     ;
 }
 
+//Compte le nombre d'arguments
 for ($tour=0; isset($table[$tour]) ; $tour++) {    
 }
 echo $tour;
---
+++ jour04/job02/index.php
@@ -0,0 +1,7 @@
+<!-- Job 02
+Développer un algorithme qui affiche dans un tableau HTML l’ensemble
+des arguments GET et les valeurs associées. Il doit y avoir deux colonnes :
+argument et valeur.
+Tips : Pour tester, créez un formulaire html de type GET avec différents
+input. -->
+
