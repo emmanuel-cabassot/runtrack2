0fa7cf41ebf65843e829f49270ca3b03e6ce92cd
emmanuel cabassot <emmanuel.cabassot@laplateforme.io>
2020-11-05 15:39:09 +0100

    job01

---
+++ jour04/job01/cible.php
@@ -0,0 +1,11 @@
+<?php
+
+foreach ($_GET as $key => $value) {
+    $table[] = $key;
+    ;
+}
+
+for ($tour=0; isset($table[$tour]) ; $tour++) {    
+}
+echo $tour;
+?>
---
+++ jour04/job01/index.php
@@ -0,0 +1,28 @@
+<!-- Job 01
+Développez un algorithme qui affiche le nombre d’arguments GET.
+Tips : Pour tester, créez un formulaire html de type GET avec différents
+input. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>   
+<div class="inscriptionlab">
+                <form  action="cible.php" method="get">
+                    
+                        <label for="name">Nom</label>
+                        <input type="text" name="nom" id="name">
+                    
+                        <label for="prenom">Prénom</label>
+                        <input type="text" name="prenom" id="prenom">
+                    
+                        <label for="date">Date de naissance</label>
+                        <input type="date" name="date" id="date">
+                        <label for="confirmaton"></label>
+                        <input type="submit" name="confirmer" id="confirmation" formaction="cible.php" inputmode="">
+</body>
+</html>
