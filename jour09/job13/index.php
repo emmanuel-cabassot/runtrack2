d171a39ee1d02bfc6698d700a9e1e4e9c8c7e4ac
emmanuel cabassot <emmanuel.cabassot@laplateforme.io>
2020-11-13 00:09:24 +0100

    fini pour de bon

---
+++ jour09/job13/index.php
@@ -0,0 +1,62 @@
+<!-- Job 13
+En utilisant php, connectez-vous à la base de données “jour08”. A l’aide
+d’une requête SQL, sélectionnez récupérer le nom des salles et le nom de
+leur étage. Affichez le résultat de cette requête dans un tableau html. La
+première ligne de votre tableau html doit contenir le nom des champs. Les
+suivantes doivent contenir les données présentes dans votre base de
+données. -->
+
+<?php
+$db = mysqli_connect("localhost", "root", "", "jour08");
+$requete = "SELECT etage.nom AS etage, salles.nom FROM salles INNER JOIN etage ON salles.id_etage = etage.id";
+// Query pour $champs
+$query = mysqli_query($db, $requete);
+$champs = mysqli_fetch_assoc($query);
+
+// Query pour $donnees
+$query_donnees = mysqli_query($db, $requete);
+$donnees = mysqli_fetch_all($query_donnees);
+
+// Compte le nombre d'occurences dans $donnees[]
+for ($ii=0; isset($donnees[$ii]); $ii++) { 
+}
+
+// Compte le nombre d'occurences dans $donnees[0][]
+for ($nbi=0; isset($donnees[0][$nbi]) ; $nbi++) { 
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+    <thead>
+        <tr>
+            <?php foreach ($champs as $key => $value) 
+            {
+            echo '<td>'.$key.'</td>';
+            }?>
+        </tr>
+    </thead>
+    <tbody>
+            <?php for ($i=0; $i < $ii ; $i++) 
+            { 
+                echo '<tr>';
+                for ($nb=0; $nb < $nbi ; $nb++) 
+                { 
+                    echo '<td>'.$donnees[$i][$nb].'</td>';
+                }
+                echo '</tr>';
+            }?>
+        </tbody>
+    </table>  
+</body>
+</html>
+<?php
+mysqli_close($db);
+?>
